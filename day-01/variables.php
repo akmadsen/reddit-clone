e310b25b6297726f34c7c4e7d64a7a8a12fdde3c
Alexander Madsen <akmadsen522@gmail.com>
2018-10-23 13:26:28 -0600

    Began work on variables

---
+++ day-01/variables.php
@@ -0,0 +1,16 @@
+<pre>
+<?php 
+
+$a = 2; 
+$b = 2.4; 
+$c = true; 
+$d = "true"; 
+$e = [1,2,"foo"]; 
+$f = new stdClass(); 
+$g = function() { 
+    return "Hello, World!"; 
+};
+
+var_dump($a,$b,$c,$d,$e,$f,$g);
+?> 
+</pre> 
