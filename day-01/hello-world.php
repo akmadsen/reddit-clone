7808fbea8faa5360f34a3efb52ea0978d3882557
Alexander Madsen <akmadsen522@gmail.com>
2018-10-23 13:10:05 -0600

    Beginning Day 1

---
+++ day-01/hello-world.php
@@ -0,0 +1,5 @@
+<?php 
+    echo "<h1 style=\"color:blue;border:2px solid red\">Hello, World!</h1>"; 
+    date_default_timezone_set ("America/Edmonton"); 
+    echo "<h2>" . date("Y-m-d H:i:s") . "</h2>";
+?>
--- index.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-    echo "<h1 style=\"color:blue;border:2px solid red\">Hello, World!</h1>"; 
-
-?>
