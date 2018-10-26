ad5b511fe9c588667876feeae1b4f9cd2062378a
Alexander Madsen <akmadsen522@gmail.com>
2018-10-26 09:24:45 -0600

    started working with the header info

--- twitter/resources/views/welcome.blade.php
+++ twitter/resources/views/welcome.blade.php
@@ -4,30 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
     <link rel="stylesheet" href="css/app.css">
     <title>Document</title>
 </head>
 <body>    
     <header class="flex flex-v">
-        <div class="top-top bg-white">
-            <nav class="flex flex-h">
-                <div class="left-section flex flex-h flex-1">
-                    <div class="home">Home</div>
-                    <div class="moments">Moments</div>
-                    <div class="notifications">Notifications</div>
-                    <div class="messages">Messages</div>
-                </div>
-
-                <div class="center-section flex-1 flex flex-h flex-j-center">
-                    <div class="twitter-bird">Twitter bird</div>
-                </div>
-
-                <div class="right-section flex-1 flex flex-h flex-j-end">
-                    <div class="search-box">Search for Stuff</div>
-                    <div class="profile-icon">Profile Image</div>
-                    <div class="tweet-button">Tweet</div>
-                </div>
-            </nav>
+        <div class="top-top bg-white height-50">
+            <div class="container height-100p">
+                <nav class="flex flex-h height-100p">
+                    <div class="left-section height-100p flex flex-h flex-1 flex-a-center">
+                        <div class="nav-item"><i class="fas fa-home"></i>Home</div>
+                        <div class="nav-item"><i class="fas fa-bolt"></i>Moments</div>
+                        <div class="nav-item"><i class="far fa-bell"></i>Notifications</div>
+                        <div class="nav-item"><i class="far fa-envelope"></i>Messages</div>
+                    </div>
+                    
+                    <div class="center-section height-100p flex-1 flex flex-h flex-j-center flex-a-center">
+                        <div class="twitter-bird"><i class="fab fa-twitter"></i></div>
+                    </div>
+                    
+                    <div class="right-section height-100p flex-1 flex flex-h flex-j-end flex-a-center">
+                        <div class="search-box">Search for Stuff</div>
+                        <div class="profile-icon">Profile Image</div>
+                        <div class="tweet-button">Tweet</div>
+                    </div>
+                </nav>
+            </div>
         </div>
         <div class="hero-row bg-2">
             <div class="hero-img">
@@ -41,7 +45,7 @@
                     <div class="label fw-bold">
                         Tweets
                     </div>
-                    <div class="value">
+                    <div class="value text-center">
                         1,966
                     </div>
                 </div>
@@ -50,7 +54,7 @@
                     <div class="label fw-bold">
                         Following
                     </div>
-                    <div class="value">
+                    <div class="value text-center">
                         71
                     </div>
                 </div>
@@ -59,7 +63,7 @@
                     <div class="label fw-bold">
                         Followers
                     </div>
-                    <div class="value">
+                    <div class="value text-center">
                         100K
                     </div>
                 </div>
@@ -68,8 +72,17 @@
                     <div class="label fw-bold">
                         Likes
                     </div>
-                    <div class="value">
+                    <div class="value text-center">
                         39
+                    </div>
+                </div>
+
+                <div class="stat">
+                    <div class="label fw-bold">
+                        Moments
+                    </div>
+                    <div class="value text-center">
+                        2
                     </div>
                 </div>
             </div>
