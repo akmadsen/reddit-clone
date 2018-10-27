bbaa1c133ee56825d9ee523eeb951e9bb2eeffcc
Alex Madsen <akmadsen522@gmail.com>
2018-10-27 14:17:09 -0600

    Have skeleton assembled for mock-reddit

--- assignment/resources/views/welcome.blade.php
+++ assignment/resources/views/welcome.blade.php
@@ -6,22 +6,70 @@
   </head>
   <body>
     <header>
-      Reddit Logo
-      Content Filter
-      Search Bar
-      Log In
-      Sign Up
-      Visit Old Reddit
-      Options Icon
-      H-Rule
-      View Options
-      Sort Options
+      <div class="top-header">
+        <div class="company">
+          Reddit Logo
+        </div>
+        <div class="filter">
+          Content Filter
+        </div>
+        <div class="search-bar">
+          Search Bar
+        </div>
+        <div class="login-buttons">
+          Log In
+          Sign Up
+        </div>
+        <div class="user-options">
+          Visit Old Reddit
+          Options Icon
+        </div>
+      </div>
+      
+      <div class="bottom-header">
+        <div class="view-options">
+          View
+          Large
+          Medium
+          Compact
+        </div>
+        <div class="sort-options">
+          Sort 
+          Hot v
+          Canada v
+        </div>
+      </div>
     </header>
 
     <main>
-      main-feed
-      sign-up cta
-      auxilliary feed
+      <div class="main-feed">
+        <div class="sign-up-cta">
+          Sign up plz!
+        </div>
+        <div class="post">
+          Hello, I'm a Post!
+        </div>
+      </div>
+      <div class="auxilliary-feed">
+        <div class="subreddit">
+          Subreddit Info Here Plz. 
+        </div>
+        <div class="advertisement">
+          Advertisement Here Plz.
+        </div>
+        <div class="reddit-premium">
+          Reddit Premium Stuff Here Plz. 
+        </div>
+        <div class="trending-communities">
+          Trending Communities Here Plz. 
+        </div>
+        <div class="advertisement">
+          Another, Separate Ad Here Plz. 
+        </div>
+        <div class="footer">
+          Reddit Footer Info Here Plz. 
+        </div>
+      </div>
     </main>
   </body>
 </html>
