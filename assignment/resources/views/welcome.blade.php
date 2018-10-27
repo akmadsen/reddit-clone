25fa4da67b329b8e39f73c94c1be7d4b6cafbbbf
Alex Madsen <akmadsen522@gmail.com>
2018-10-27 15:23:51 -0600

    Working on the header some

--- assignment/resources/views/welcome.blade.php
+++ assignment/resources/views/welcome.blade.php
@@ -2,27 +2,41 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/app.css">
+
     <title>CPNT 262 - PHP - Assignment 1</title>
   </head>
   <body>
     <header>
       <div class="top-header">
-        <div class="company">
-          Reddit Logo
+        <div class="th-left">
+          <div class="company">
+            <i class="fab fa-reddit logo"></i>
+            <span>reddit</span>
+          </div>
+          <div class="th-filter">
+            <i class="fas fa-chart-line"></i>
+            <span>Popular</span>
+            <i class="fas fa-sort-down"></i>
+          </div>
         </div>
-        <div class="filter">
-          Content Filter
+        <div class="th-mid">
+          <div class="search-bar">
+            Search Bar
+          </div>
         </div>
-        <div class="search-bar">
-          Search Bar
-        </div>
-        <div class="login-buttons">
-          Log In
-          Sign Up
-        </div>
-        <div class="user-options">
-          Visit Old Reddit
-          Options Icon
+        <div class="th-right">
+          <div class="login-buttons">
+            Log In
+            Sign Up
+          </div>
+          <div class="user-options">
+            Visit Old Reddit
+            Options Icon
+          </div>
         </div>
       </div>
       
