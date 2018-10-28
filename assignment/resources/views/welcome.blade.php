1c6a99e62d49a9f4b3a0ff1a702e3f48069b4e51
Alex Madsen <akmadsen522@gmail.com>
2018-10-28 16:22:09 -0600

    going to call the bottom header acceptable for now

--- assignment/resources/views/welcome.blade.php
+++ assignment/resources/views/welcome.blade.php
@@ -50,21 +50,26 @@
       </div>
       
       <div class="bottom-header">
-        <div class="view-options">
-          <span>View</span>
-          <i class="fas fa-th"></i>
-          <i class="fas fa-th-large"></i>
-          <i class="fas fa-th-list"></i>
-        </div>
-        <div class="sort-options">
-          <span>Sort</span>  
-          <div class="bh-filter-type">
-            <span>Hot</span>
-            <i class="fas fa-sort-down"></i>
+        <div class="container">
+          <div class="view-options">
+            <span class="option-title">View</span>
+            <div class="view-opt-icons">
+              <i class="fas fa-th selected"></i>
+              <i class="fas fa-th-large deselected"></i>
+              <i class="fas fa-th-list deselected"></i>
+            </div>
           </div>
-          <div class="bg-filter-location">
-            <span>Canada</span>
-            <i class="fas fa-sort-down"></i>
+          <div class="sort-options">
+            <span class="option-title">Sort</span>  
+            <div class="bh-filter-type bh-filter-list">
+              <i class="fas fa-fire flame"></i>
+              <span class="sort-option">Hot</span>
+              <i class="fas fa-sort-down list-arrow"></i>
+            </div>
+            <div class="bh-filter-location bh-filter-list">
+              <span class="sort-option">Canada</span>
+              <i class="fas fa-sort-down list-arrow"></i>
+            </div>
           </div>
         </div>
       </div>
