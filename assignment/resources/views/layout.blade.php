6167f01c7402b7daa3cc30f1fcb700b47ed4317f
Alexander Madsen <akmadsen522@gmail.com>
2018-10-29 15:24:11 -0600

    Fixed weird formatting issue

--- assignment/resources/views/layout.blade.php
+++ assignment/resources/views/layout.blade.php
@@ -2,32 +2,34 @@
 <html lang="en" dir="ltr">
 
 <head>
-  <meta charset="utf-8">
+    <meta charset="utf-8">
 
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns"
-    crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns"
+        crossorigin="anonymous">
 
-  <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/app.css">
 
-  <title>CPNT 262 - PHP - Assignment 1</title>
+    <title>CPNT 262 - PHP - Assignment 1</title>
 </head>
 
 <body>
-  @include('header')
-  <main>
-    <div class="container">
-      <div class="main-feed">
-        
-        @yield('main-feed-content')
+    
+    @include('header')
 
-      </div>
-      <div class="auxilliary-feed">
+    <main>
+        <div class="container">
+            <div class="main-feed">
 
-        @yield('aux-feed-content')
+                @yield('main-feed-content')
 
-      </div>
-    </div>
-  </main>
+            </div>
+            <div class="auxilliary-feed">
+
+                @yield('aux-feed-content')
+
+            </div>
+        </div>
+    </main>
 </body>
 
 </html>
--- assignment/resources/views/welcome.blade.php
+++ assignment/resources/views/welcome.blade.php
@@ -1,18 +1,17 @@
 @extends('layout') 
 
 @section('main-feed-content')
-  @include('sign-up-cta')
-  <?php foreach($posts as $post): ?>
-    @include('main-feed-post')
-  <?php endforeach; ?>
+    @include('sign-up-cta')
+    <?php foreach($posts as $post): ?>
+        @include('main-feed-post')
+    <?php endforeach; ?>
 @endsection
-
-
+ 
 @section('aux-feed-content')
-  @include('subreddit-post')
-  @include('advertisement-post')
-  @include('reddit-premium-post')
-  @include('trending-communities-post')
-  @include('advertisement-post')
-  @include('footer-post')
+    @include('subreddit-post')
+    @include('advertisement-post')
+    @include('reddit-premium-post')
+    @include('trending-communities-post')
+    @include('advertisement-post')
+    @include('footer-post')
 @endsection
