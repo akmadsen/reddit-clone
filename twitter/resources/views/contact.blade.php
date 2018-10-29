f317eddb02ef107923ca07f6eee6f80aaed1a219
Alexander Madsen <akmadsen522@gmail.com>
2018-10-29 11:07:40 -0600

    Beginning to work with layout as well... though things got busted

--- twitter/resources/views/contact.blade.php
+++ twitter/resources/views/contact.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact Page</title>
-</head>
-<body>
-    <h1>I'm a Contact Page</h1>
-    <p>Contact me, I dare you.</p>
-</body>
-</html>
+@extends('layout')
+
+@section('content')
+    <h1>Contact form here</h1>   
+@endsection
