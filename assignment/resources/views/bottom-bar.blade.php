251dddadf12350428c2a314f2b35f3485abf385c
Alexander Madsen <akmadsen522@gmail.com>
2018-10-30 15:11:02 -0600

    Broke up the header file into two sub-header files

---
+++ assignment/resources/views/bottom-bar.blade.php
@@ -0,0 +1,24 @@
+<div class="bottom-header">
+    <div class="wrapper">
+        <div class="view-options">
+            <span class="option-title">View</span>
+            <div class="view-opt-icons">
+                <i class="fas fa-th selected"></i>
+                <i class="fas fa-th-large deselected"></i>
+                <i class="fas fa-th-list deselected"></i>
+            </div>
+        </div>
+        <div class="sort-options">
+            <span class="option-title">Sort</span>
+            <div class="bh-filter-type bh-filter-list">
+                <i class="fas fa-fire flame"></i>
+                <span class="sort-option">Hot</span>
+                <i class="fas fa-sort-down list-arrow"></i>
+            </div>
+            <div class="bh-filter-location bh-filter-list">
+                <span class="sort-option">Canada</span>
+                <i class="fas fa-sort-down list-arrow"></i>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ assignment/resources/views/top-bar.blade.php
@@ -0,0 +1,37 @@
+<div class="top-header">
+    <div class="th-left">
+        <div class="company">
+            <i class="fab fa-reddit logo"></i>
+            <span>reddit</span>
+        </div>
+        <div class="th-filter">
+            <i class="fas fa-chart-line trend-line"></i>
+            <span>Popular</span>
+            <i class="fas fa-sort-down open-list"></i>
+        </div>
+    </div>
+    <div class="th-mid">
+        <div class="search-bar">
+            <i class="fas fa-search search-glass"></i>
+            <span>Search Reddit</span>
+        </div>
+        <div class="search-filters">
+            <i class="fas fa-chart-bar search-filter-icon"></i>
+            <i class="fas fa-chart-line search-filter-icon"></i>
+            <i class="fas fa-archive search-filter-icon"></i>
+        </div>
+    </div>
+    <div class="th-right">
+        <div class="login-buttons">
+            <button class="login-button">LOG IN</button>
+            <button class="signin-button">SIGN UP</button>
+        </div>
+        <div class="old-reddit-link">
+            <a href="#"><i class="fas fa-external-link-alt"></i>Visit Old Reddit</a>
+        </div>
+        <button class="user-options">
+            <i class="fas fa-user-alt usr-img"></i>
+            <i class="fas fa-sort-down usr-open"></i>
+          </button>
+    </div>
+</div>
