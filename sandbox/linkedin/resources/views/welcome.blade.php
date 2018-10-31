7afa5b33a1f419fb155a914bf89af466858b31c5
Alexander Madsen <akmadsen522@gmail.com>
2018-10-31 13:00:48 -0600

    Started sketching out the linkedin site

--- sandbox/linkedin/resources/views/welcome.blade.php
+++ sandbox/linkedin/resources/views/welcome.blade.php
@@ -4,39 +4,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="css/app.css">
+
     <title>Fake LinkedIn</title>
 </head>
 <body>
     <header>
-        LinkedIn Header
+        <div class="wrapper">
+            LinkedIn Header
+        </div>
     </header>
     <section class="sub-header-ad">
-        Sub-Header Advertisement Here 
+        <div class="wrapper">
+            Sub-Header Advertisement Here 
+        </div>
     </section>
     <main>
-        <div class="column-left">
-            <div class="profile">
-                User Profile Stuff goes Here
+        <div class="wrapper">
+            <div class="column-left">
+                <div class="profile">
+                    User Profile Stuff goes Here
+                </div>
             </div>
-        </div>
-        <div class="column-center">
-            <div class="write-post">
-                Write a Post
+            <div class="column-center">
+                <div class="write-post">
+                    Write a Post
+                </div>
+                <div class="post-feed">
+                    This is the post feed
+                    
+                </div>
             </div>
-            <div class="post-feed">
-                This is the post feed
-
-            </div>
-        </div>
-        <div class="column-right">
-            <div class="trending-topics">
-                Trending Topics go Here
-            </div>
-            <div class="sub-ad">
-                Advertisement Space Here
-            </div>
-            <div class="li-footer">
-                Linked-in footer goes here
+            <div class="column-right">
+                <div class="trending-topics">
+                    Trending Topics go Here
+                </div>
+                <div class="sub-ad">
+                    Advertisement Space Here
+                </div>
+                <div class="li-footer">
+                    Linked-in footer goes here
+                </div>
             </div>
         </div>
     </main>
