44fd1305855c9fe976a6597302c96d9df794f200
Alex Madsen <akmadsen522@gmail.com>
2018-11-04 16:45:09 -0700

    Header now successfully checks for authorization

---
+++ assignment/resources/views/login-buttons.blade.php
@@ -0,0 +1,11 @@
+<div class="login-buttons">
+    <a class="login-button" href="/login">LOG IN</a>
+    <a class="signin-button" href="/register">SIGN UP</a>
+</div>
+<div class="old-reddit-link">
+    <a href="#"><i class="fas fa-external-link-alt"></i>Visit Old Reddit</a>
+</div>
+<button class="user-options">
+    <i class="fas fa-user-alt usr-img"></i>
+    <i class="fas fa-sort-down usr-open"></i>
+</button>
--- assignment/resources/views/top-bar.blade.php
+++ assignment/resources/views/top-bar.blade.php
@@ -22,16 +22,10 @@
         </div>
     </div>
     <div class="th-right">
-        <div class="login-buttons">
-            <a class="login-button" href="/login">LOG IN</a>
-            <a class="signin-button" href="/register">SIGN UP</a>
-        </div>
-        <div class="old-reddit-link">
-            <a href="#"><i class="fas fa-external-link-alt"></i>Visit Old Reddit</a>
-        </div>
-        <button class="user-options">
-            <i class="fas fa-user-alt usr-img"></i>
-            <i class="fas fa-sort-down usr-open"></i>
-          </button>
+        <?php if (!Auth::check()): ?> 
+            @include('login-buttons')
+        <?php else: ?>
+        <span>Welcome {{ request()->user()->name }}</span> 
+        <?php endif; ?> 
     </div>
 </div>
