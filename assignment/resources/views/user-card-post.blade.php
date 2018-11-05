c9b43750244ed43275d2bf917010608373750926
Alex Madsen <akmadsen522@gmail.com>
2018-11-04 20:56:52 -0700

    Now able to view each user's profile

---
+++ assignment/resources/views/user-card-post.blade.php
@@ -0,0 +1,29 @@
+<div class="user-card-post">
+    <?php if($user->profile->hero_image): ?>
+        <img class="user-hero" src="#" alt="User hero image.">
+    <?php else: ?>
+        <div class="default-user-hero"></div>
+    <?php endif; ?>
+    
+    <img
+        class="user-icon" 
+        src="{{ $user->profile->icon ? 
+            $user->profile->icon : 
+            'https://i.redd.it/130am13nj6201.png' }}" 
+        alt="User icon image."
+    />
+    <span class="handle">u/{{ $user->profile->handle }}</span>
+    
+    <?php if($user->profile->description): ?>
+        <p class="description">Description.</p>
+    <?php endif; ?>
+    
+    <div class="cake-day">
+        <p class="cd-title">Cake Day</p>
+        <p class="cd-date">
+            <i class="fas fa-birthday-cake"></i>
+            <span>{{ $user->created_at->format('F d, Y')}}</span>
+        </p>
+    </div>
+    <a class="new-post-button" href="#">New Post</a>
+</div>
--- assignment/resources/views/user-profile.blade.php
+++ assignment/resources/views/user-profile.blade.php
@@ -11,23 +11,6 @@
 @endsection 
 
 @section('aux-feed-content') 
-    <div class="user-card-post">
-        <?php if($user->profile->hero_image): ?>
-            <img class="user-hero" src="#" alt="User hero image.">
-        <?php else: ?>
-            <div class="default-user-hero"></div>
-        <?php endif; ?>
-        
-        <img
-            class="user-icon" 
-            src="{{ $user->profile->icon ? 
-                $user->profile->icon : 
-                'https://i.redd.it/130am13nj6201.png' }}" 
-            alt="User icon image."
-        />
-        <span class="handle">u/{{ $user->profile->handle }}</span>
-        <p class="description">Description.</p>
-        <a class="new-post-button" href="#">New Post</a>
-    </div>
+    @include('user-card-post') 
     @include('footer-post') 
 @endsection
