1a048a35e7bfab11426ae65c2e65f73a53352d7c
Alex Madsen <akmadsen522@gmail.com>
2018-11-04 23:00:57 -0700

    Retrofitted a logout button into the top-bar

--- assignment/resources/views/top-bar.blade.php
+++ assignment/resources/views/top-bar.blade.php
@@ -26,9 +26,22 @@
             @include('login-buttons')
         <?php else: ?>
             <a href="/edit_profile">Edit Profile</a>
+            <div>
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </div>
             <a href="/u/<?php echo request()->user()->profile->handle ?>">
                 <span>Welcome, {{ request()->user()->profile->handle }}</span> 
             </a>
+            
+
         <?php endif; ?> 
     </div>
 </div>
